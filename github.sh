4d85b33b5de9f5a2455c9e3e6a04e0a21cec34b2
ivanlppires <ivanpires@gmail.com>
2024-11-06 21:12:26 -0400

    SWAGGER o GET E DELETE

--- github.sh
+++ github.sh
@@ -1,9 +1,6 @@
-#git init
 git add .
 git commit -m "SWAGGER o GET E DELETE"
-#git branch -M main
-#git remote add origin https://github.com/ivanlppires/FMDS_2024-2_AULA-14.git
-git push -u origin main
+git push -u origin main --force
 
 
 

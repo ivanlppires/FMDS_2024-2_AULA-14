e026b685b758bdb36cba9750e6ad9fe4e12543f7
ivanlppires <ivanpires@gmail.com>
2024-11-06 21:11:19 -0400

    SWAGGER o GET E DELETE

--- github.sh
+++ github.sh
@@ -1,7 +1,6 @@
-echo "# FMDS_2024-2_AULA-14" >> README.md
 #git init
 git add .
-git commit -m "CRUD EXPRESS COM MYSQL DOCUMENTADO COM SWAGGER o GET E DELETE"
+git commit -m "SWAGGER o GET E DELETE"
 #git branch -M main
 #git remote add origin https://github.com/ivanlppires/FMDS_2024-2_AULA-14.git
 git push -u origin main
